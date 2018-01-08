100b5d8b6e60e2c508b176c0cbfb1d2073b79a7c
SageSingularity <34374232+SageSingularity@users.noreply.github.com>
2018-01-07 22:55:14 -0600

    Update calculate_path.py
    First working version!

--- calculate_path.py
+++ calculate_path.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 class CalculatePath():
-    """ Calculates shortest path to destination on a Chess Board """
+    """ Worker class for calculating the shortest route """
     def __init__(self):
         # The chess board, mapped to a 2D List
         self.grid = [[ 0, 1, 2, 3, 4, 5, 6, 7 ],
@@ -16,72 +16,17 @@ class CalculatePath():
                      [48,49,50,51,52,53,54,55 ],
                      [56,57,58,59,60,61,62,63 ]]
                      
-       # How the knight moves on the Board
-       self.knight_moves = {"up2-left1": [-2, -1], "up2-right1": [-2, 1],
-                            "down2-left1": [2, -1], "down2-right1": [2, 1],
-                            "left2-up1": [-1, -2], "left2-down1": [1, -2],
-                            "right2-up1": [-1, 2], "right2-down1": [1, 2]}
-                            
-                            
-   def _explore_current_level(starting_point, possible_moves)
-       """ Discover possible moves from current location that respect board boundaries
+        # How the knight moves on the Board
+        self.knight = {"up2-left1": [-2, -1], "up2-right1": [-2, 1],
+                       "down2-left1": [2, -1], "down2-right1": [2, 1],
+                       "left2-up1": [-1, -2], "left2-down1": [1, -2],
+                       "right2-up1": [-1, 2], "right2-down1": [1, 2]}
 
-       Input:
-           starting_point - The current location on the board
-           possible_moves - Piece movement capability, i.e. self.knight_moves
-           self.grid - The 2D list representing the board
-       Output:
-           available_moves - A list of moves possible from the current location
-       """
-       available_moves = []
-
-       # Find the coordinates of the starting point in the chess grid
-       grid_row, grid_column = _find_index(starting_point, self.grid)
-
-       for move in possible_moves:
-           try:
-               if grid_row + possible_moves[move][0] < 0:
-                   # Do not allow negative indices
-                   continue
-               elif grid_column + possible_moves[move][1] < 0
-                   # Do not allow negative indices
-                   continue
-               possible = self.grid[grid_row + possible_moves[move][0]][grid_column + possible_moves[move][1]]
-               available_moves.append(possible)
-           except:
-               # Move not possible!
-               pass
-
-        return available_moves
-
-
-    def _find_index(board_position):
-        """ This function locates the position of a number on the chess grid
-
-        Input:
-            board_position - An integer representing a position on board (0-63)
-            self.grid - The 2D list representing the board
-        Output:
-            row, column - The row and column in the 2D list that matches the position
-                          number on the board
-        """
-        for row_iteration in self.grid:
-            try:
-                column = row_iteration.index(board_position)
-                row = self.grid.index(row_iteration)
-                return row, column
-            except:
-                # Didn't find it in this row, continue
-                pass
-
-
-    def breadth_first(start, target, possible_moves):
+    def breadth_first(self, chess_piece, start, target):
         """ Performs breadth first search.
-
         An expedient way to find the shortest route, is to explore all possible routes
         from our current "depth" and check for the target destination in this list. This
         is performed many times until the destination is discovered.
-
         Input:
             start - The starting point given for the chess piece
             target - The destination given for the chess piece
@@ -94,7 +39,11 @@ class CalculatePath():
         count = 1
         depth = 0
         queue = []
-        destinations = explore_current_level(start, possible_moves)
+
+        possible_moves = self.__dict__[chess_piece]
+
+        # Setup the starting conditions
+        destinations = self._explore_current_level(start, possible_moves)
 
         for dest in destinations:
             list_of_paths.append([dest])
@@ -107,9 +56,45 @@ class CalculatePath():
                 if target in value:
                     break_flag = True
                     break
-                destinations = explore_current_level(value[-1], possible_moves)
+                destinations = self._explore_current_level(value[-1], possible_moves)
                 for dest in destinations:
                     list_of_paths.append(value + [dest])
             if break_flag == True:
                 break
-        return list_of_paths
+        return list_of_paths             
+
+
+    def _explore_current_level(self, starting_point, possible_moves):
+        """ Discover possible moves from current location that respect board boundaries
+        Input:
+            starting_point - The current location on the board
+            possible_moves - Piece movement capability, i.e. self.knight
+            self.grid - The 2D list representing the board
+        Output:
+            available_moves - A list of moves possible from the current location
+        """
+        available_moves = []
+        # Find the coordinates of the starting point in the chess grid
+        for row_iteration in self.grid:
+            try:
+                grid_column = row_iteration.index(starting_point)
+                grid_row = self.grid.index(row_iteration)
+            except:
+                # Didn't find it in this row, continue
+                pass
+
+        for move in possible_moves:
+            try:
+                if grid_row + possible_moves[move][0] < 0:
+                    # Do not allow negative indices
+                    continue
+                elif grid_column + possible_moves[move][1] < 0:
+                    # Do not allow negative indices
+                    continue
+                possible = self.grid[grid_row + possible_moves[move][0]][grid_column + possible_moves[move][1]]
+                available_moves.append(possible)
+            except:
+                # Move not possible!
+                pass
+
+        return available_moves
